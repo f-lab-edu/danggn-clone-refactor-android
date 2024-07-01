3b62fd155bc7b097d823decb618ae7f5a043bf01
sszz07 <sszz07@naver.com>
2024-07-01 18:10:11 +0900

    멀티이미지 저장하기

--- app/src/main/java/com/example/freemarket/myProduct/ProductSelectUpdateActivity.kt
+++ app/src/main/java/com/example/freemarket/myProduct/ProductSelectUpdateActivity.kt
@@ -6,16 +6,30 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.freemarket.R
+import com.example.freemarket.databinding.ActivityProductAddBinding
+import com.example.freemarket.databinding.ActivityProductSelectUpdateBinding
 
 class ProductSelectUpdateActivity : AppCompatActivity() {
+    private val binding: ActivityProductSelectUpdateBinding by lazy {
+        ActivityProductSelectUpdateBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_product_select_update)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
+
+        //데이터 담기
+        val productSubject = intent.getStringExtra("productSubject")!!
+        val productPrice = intent.getStringExtra("productPrice")!!
+        val productLocation = intent.getStringExtra("productLocation")!!
+        val productContent = intent.getStringExtra("productContent")!!
+
+
+        //데이터 보여주기
+        binding.tvProductSelectUpdateSubject.setText(productSubject)
+        binding.tvProductSelectUpdatePrice.setText(productPrice)
+        binding.tvProductSelectUpdateLocation.setText(productLocation)
+        binding.tvProductSelectUpdateContent.setText(productContent)
+
     }
 }
